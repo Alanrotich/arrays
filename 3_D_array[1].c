a9e1a49ce13b9d432ceabdf36e93419cb3179ab2
Alanrotich <kendaalan6@gmail.com>
2024-10-13 19:34:28 +0300

    Add files via upload

---
+++ 3_D_array[1].c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    // Declaring and initializing a 3D array with values
+    int array[2][2][2] = {
+        {
+            {1, 2},
+            {3, 4}
+        },
+        {
+            {5, 6},
+            {7, 8}
+        }
+    };
+
+    // Displaying the 3D array
+    printf("The 3D array is:\n");
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            for (int k = 0; k < 2; k++) {
+                printf("array[%d][%d][%d] = %d\n", i, j, k, array[i][j][k]);
+            }
+        }
+    }
+
+    return 0;
+}
